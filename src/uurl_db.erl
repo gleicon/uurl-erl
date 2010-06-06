6f7e6b3ce7465904713505b86b973a819b08fc7c
Gleicon Moraes <gleicon@gmail.com>
2010-06-06 19:37:25 -0300

    cleanup, css fixes, mongodb fixes

--- src/uurl_db.erl
+++ src/uurl_db.erl
@@ -5,80 +5,88 @@
 
 -module(uurl_db).
 -author('gleicon <gleicon@gmail.com>').
--export([create/3, update/4, stats/1, check_url/1, translate_url/1]).
+-export([create/1, update/3, stats/1, check_url/1, translate_url/3, datetime_helper/1]).
 
 -define (UNIQUE_ID_REF, "URLSHORT:UNIQ").
 -define (OBJECT_ID_REF, "OBJID:").
 -define(AS_JSON(K, V), bson_to_json(proplists:get_value(K, V))).
 
-create(URL, Referer, Visitor) ->
-	Id = create_unique_id(),
-	UID = base62:encode(Id),
-	emongo:insert(uurl_pool, "urls", [{"uuid", Id}, {"uurl", UID}, {"url", URL}, {"clicks", 0}, {"referers", [Referer]}, 
-		{"visitors", [Visitor]}, {"clicks_per_minute", []}, {"date", calendar:now_to_datetime(now())}]),
-	cache_url(URL, UID),
-	{ok, UID}.
+create(URL) ->
+    Id = create_unique_id(),
+    UID = base62:encode(Id),
+    Dtnow = calendar:now_to_datetime(now()),
+    emongo:insert(uurl_pool, "urls", [{"uuid", Id}, {"uurl", UID}, {"url", URL}, {"clicks", 0}, {"date", Dtnow}]),
+    cache_url(URL, UID),
+    {ok, UID}.
 
-update(UID, URL, Referer, Visitor) ->
-	Id = base62:decode(UID),
-	emongo:update(uurl_pool, "urls", [{"uuid", Id}], [
-										{"$inc", [{"clicks", 1}]},
-										{"$inc", [{["clicks_per_minutea", get_current_date()], 1} ]},
-										{"$push", [{"referers", Referer}, {"visitors", Visitor} ] }], true),
-	{ok, UID}.
+update(UID, Referer, Visitor) ->
+    Id = base62:decode(UID),
+    emongo:update(uurl_pool, "urls", [{"uuid", Id}], [
+        {"$inc", [{"clicks", 1}]},
+        {"$inc", [{["clicks_per_hour", get_current_date()], 1} ]},
+        {"$push", [{"referers", Referer}, {"visitors", Visitor} ] }], true),
+    {ok, Id}.
 
 stats(UID) -> 
-	Id = base62:decode(UID),
-	Stats = lists:flatten(emongo:find(uurl_pool, "urls", [{"uuid", Id}])),
-	Obj = {struct, [
-	    {url, ?AS_JSON(<<"url">>, Stats)},
-	    {clicks, ?AS_JSON(<<"clicks">>, Stats)},
-	    {cpm, ?AS_JSON(<<"clicks_per_minutea">>, Stats)},
-	    {visitors, ?AS_JSON(<<"visitors">>, Stats)},
-	    {referers, ?AS_JSON(<<"referers">>, Stats)}
-		]},
-	mochijson2:encode(Obj).
+    Id = base62:decode(UID),
+    Stats = lists:flatten(emongo:find(uurl_pool, "urls", [{"uuid", Id}])),
+    Obj = {struct, [
+        {url, ?AS_JSON(<<"url">>, Stats)},
+        {clicks, ?AS_JSON(<<"clicks">>, Stats)},
+        {cph, ?AS_JSON(<<"clicks_per_hour">>, Stats)},
+        {visitors, ?AS_JSON(<<"visitors">>, Stats)},
+        {referers, ?AS_JSON(<<"referers">>, Stats)}
+            ]},
+    mochijson2:encode(Obj).
 	
 
-translate_url(UID) ->
-	Id = base62:decode(UID),
-	Stats = emongo:find(uurl_pool, "urls", [{"uuid", Id}]),	
-	URL = proplists:get_value(list_to_bitstring("url"), lists:flatten(Stats)),
-	bitstring_to_list(URL).
-	
+translate_url(UID, Referer, Visitor) ->
+    {ok, Id} = update(UID, Referer, Visitor), 
+    Stats = emongo:find(uurl_pool, "urls", [{"uuid", Id}]),	
+    URL = proplists:get_value(list_to_bitstring("url"), lists:flatten(Stats)),
+    bitstring_to_list(URL).
+
+
 get_current_date() ->
-		{Date, Time}=calendar:now_to_datetime(now()),
-		DP = lists:map(fun(Dt) -> "." ++ integer_to_list(Dt) end, tuple_to_list(Date)),
-		TP = lists:map(fun(Tm) -> "." ++ integer_to_list(Tm) end, tuple_to_list(Time)),
-		lists:flatten(DP++lists:sublist(TP, 1, 2)).
+    {Date, Time}=calendar:now_to_datetime(now()),
+    DP = lists:map(fun(Dt) -> "." ++ integer_to_list(Dt) end, tuple_to_list(Date)),
+    TP = lists:map(fun(Tm) -> "." ++ integer_to_list(Tm) end, tuple_to_list(Time)),
+    lists:flatten(DP++lists:sublist(TP, 1, 1)).
 	
 create_unique_id() ->
-	{ok, Client} = erldis:connect("localhost", 6379),
-	case erldis:incr(Client, list_to_bitstring(?UNIQUE_ID_REF)) of
-		true ->
-			Id2 = 1;
-		Id ->
-	        Id2 = Id
-	end,
-	erldis_client:stop(Client),
+    {ok, Client} = erldis:connect("localhost", 6379),
+    case erldis:incr(Client, list_to_bitstring(?UNIQUE_ID_REF)) of
+            true ->
+                    Id2 = 1;
+            Id ->
+            Id2 = Id
+    end,
+    erldis_client:stop(Client),
 	Id2.
 	
 check_url(Url) ->
-	{ok, Client} = erldis:connect("localhost", 6379),
-	case erldis:get(Client, list_to_bitstring(?OBJECT_ID_REF++Url)) of 
-		nil ->
-			Val = nil;
-		V -> 
-			Val = bitstring_to_list(V)
-		end,
-	erldis_client:stop(Client),
-	Val.
-	
+    {ok, Client} = erldis:connect("localhost", 6379),
+    case erldis:get(Client, list_to_bitstring(?OBJECT_ID_REF++Url)) of 
+            nil ->
+                    Val = nil;
+            V -> 
+                    Val = bitstring_to_list(V)
+            end,
+    erldis_client:stop(Client),
+    Val.
+    
 cache_url(Url, Eid) ->
-	{ok, Client} = erldis:connect("localhost", 6379),
-	Val = erldis:set(Client, list_to_bitstring(?OBJECT_ID_REF++Url), Eid),
-	erldis_client:stop(Client),
-	Val.
+    {ok, Client} = erldis:connect("localhost", 6379),
+    Val = erldis:set(Client, list_to_bitstring(?OBJECT_ID_REF++Url), Eid),
+    erldis_client:stop(Client),
+    Val.
+
+
+datetime_helper(Local_time) when Local_time == [] -> "nan";
+datetime_helper(Local_time) ->
+        {{Year, Month, Day}, {Hour, Min, Sec}} = Local_time,
+            lists:flatten(io_lib:fwrite("~2B/~2B/~4..0B ~2B:~2.10.0B:~2.10.0B",
+                                        [Day, Month, Year, Hour, Min, Sec])).
 
 %% BSON2JSON stuff from davisp / freenode's #erlang
 
--- src/uurl_web.erl
+++ src/uurl_web.erl
@@ -36,50 +36,60 @@ loop(Req, DocRoot) ->
                 "favicon.ico" ->
                     Req:serve_file("favicon.ico", DocRoot);
                 "index.html" ->
-	                Req:serve_file("index.html", DocRoot);
-				[] ->
-		            Req:serve_file("index.html", DocRoot);
-				_ ->
-					case string:str(Path, "stats") of
-						1 ->
-							UID = string:sub_string(Path, length(?STATS_RESOURCE)+1),
-							Stats = uurl_db:stats(UID),
-							Req:ok({"text/plain", [], [Stats]});			
-						_ ->
-							case lists:last(Path) of 
-								$! ->
-									%% ! at the end of URL means stats
-									%%Stats = uurl_db:stats(string:strip(Path, right, $!)),
-									O = stats:render(string:strip(Path, right, $!)),
-%%									Req:ok({"text/html", [], ["<h1>stats</h1>"++string:strip(Path, right, $!)]});
-									Req:ok({"text/html", [], O});
+                    Req:serve_file("index.html", DocRoot);
+	        [] ->
+		    Req:serve_file("index.html", DocRoot);
+	        _ ->
+		    case string:str(Path, "stats") of
+		        1 ->
+			    UID = string:sub_string(Path, length(?STATS_RESOURCE)+1),
+			    Stats = uurl_db:stats(UID),
+                            case proplists:get_value("jsoncallback", Req:parse_qs()) of
+                                undefined ->
+                                    Req:ok({"text/plain", [], [Stats]});
+                                Val ->
+                                    Req:ok({"text/plain", [], [Val++"("++Stats++")"]})			
+                            end;
+                        _ ->
+                            case lists:last(Path) of 
+                                $! ->
+                                    %% ! at the end of URL means stats
+                                    O = stats:render(string:strip(Path, right, $!)),
+                                    Req:ok({"text/html", [], O});
 
-								_ ->
-									Req:respond({302, [{"Location", uurl_db:translate_url(Path)}, {"Content-Type", "text/html; charset=UTF-8"}],""})
-							end
-	                    		%%Req:not_found()
+                                _ ->
+                                    case uurl_db:translate_url(Path, Req:get_header_value("HTTP_REFERER"), Req:get(peer)) of
+                                        not_found ->
+                                            Req:not_found();
+                                        Url ->
+                                            Req:respond({302, [{"Location", Url}, 
+                                                {"Content-Type", "text/html; charset=UTF-8"}],""})
+                            end
+                        end
                     end
 		end;
         'POST' ->
             case Path of
                 _ ->
-					Params = Req:parse_post(),
-					U = proplists:get_value("u", Params),
-					case {string:str(U, "http://"), string:str(U, "https://")} of
-						{0,0} ->
-							URL = "http://" ++ U;
-						_ ->
-							URL = U
-						end,
+                    Params = Req:parse_post(),
+	            U = proplists:get_value("u", Params),
+	            case {string:str(U, "http://"), string:str(U, "https://")} of
+	                {0,0} ->
+	                    URL = "http://" ++ U;
+		        _ ->
+		            URL = U
+		    end,
 						
-					case uurl_db:check_url(URL) of 
-						nil ->
-							{ok, UID} = uurl_db:create(U, Req:get_header_value("HTTP_REFERER"), Req:get(peer));
-						Id ->
-							{ok, UID} = uurl_db:update(Id, U, Req:get_header_value("HTTP_REFERER"), Req:get(peer))
-						end,
+	            case uurl_db:check_url(URL) of 
+		        nil ->
+		            {ok, UID} = uurl_db:create(URL);
+		        Id ->
+		            {ok, UID} = uurl_db:update(Id, Req:get_header_value("HTTP_REFERER"), Req:get(peer))
+		    end,
+     
+                    O = stats:render(UID),
+                    Req:ok({"text/html", [], O})
 							
-                    Req:ok({"text/html", [], ["<h1>Cool!</h1>"++URL++"<BR>"++UID]})
             end;
         _ ->
             Req:respond({501, [], []})
